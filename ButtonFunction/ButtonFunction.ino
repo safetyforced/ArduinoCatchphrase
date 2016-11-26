c9248dd94b2b514182aacb1d42a2c7add7af8684
Alex M <safetyforced@gmail.com>
2016-11-26 16:04:18 -0500

    parameterized debounce just in case

--- ButtonFunction/ButtonFunction.ino
+++ ButtonFunction/ButtonFunction.ino
@@ -17,33 +17,33 @@ pinMode(ledPin1, OUTPUT);
 pinMode(ledPin2, OUTPUT);
 }
 
-boolean debounce1(boolean last) {
-  boolean current = digitalRead(switchPin1);
+boolean debounce(boolean last, char button) {
+  boolean current = digitalRead(button);
   if (last != current){
     delay(5);
-    current = digitalRead(switchPin1);
+    current = digitalRead(button);
   }
   return current;
 }
-boolean debounce2(boolean last) {
-  boolean current = digitalRead(switchPin2);
-  if (last != current){
-    delay(5);
-    current = digitalRead(switchPin2);
-  }
-  return current;
-}
+//boolean debounce2(boolean last) {
+//  boolean current = digitalRead(switchPin2);
+//  if (last != current){
+//    delay(5);
+//    current = digitalRead(switchPin2);
+//  }
+//  return current;
+//}
 
 void loop() {
   // put your main code here, to run repeatedly:
-  currentButton1 = debounce1(lastButton1);
+  currentButton1 = debounce(lastButton1, switchPin1);
 if (currentButton1 == HIGH  && lastButton1 == LOW) {
 ledOn1 = !ledOn1;
 }
 lastButton1 = currentButton1;
   digitalWrite(ledPin1, ledOn1);
   
-    currentButton2 = debounce2(lastButton2);
+    currentButton2 = debounce(lastButton2, switchPin2);
 if (currentButton2 == HIGH  && lastButton2 == LOW) {
 ledOn2 = !ledOn2;
 }
